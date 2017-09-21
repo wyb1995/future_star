32cf607ad5c0e4005f4ba0a0b778cb5a12a7060c
wang yuanbo <ybowang@CNybowang.local>
2017-09-21 12:08:41 +0800

    [Yuanbo]refactor code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,30 +1,35 @@
-import org.omg.CORBA.INTERNAL;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
 class Calculator {
-    int sum(List<Integer> integers) {
-        return integers.stream().filter(item -> item % 2 == 0).mapToInt(Integer::intValue).sum();
+    static int sum(List<Integer> numbers) {
+        return numbers.stream().filter(item -> item % 2 == 0).mapToInt(Integer::intValue).sum();
     }
 
-    Map<String, Integer> sort(List<String> strings) {
-        Collections.sort(strings);
-        Map<String, Integer> map = new LinkedHashMap<>();
-        strings.forEach(item -> map.put(item, item.length()));
-        return map;
+    static Map<String, Integer> sort(List<String> emailNames) {
+        Collections.sort(emailNames);
+
+        Map<String, Integer> output = new LinkedHashMap<>();
+
+        emailNames.forEach(item -> output.put(item, item.length()));
+        return output;
     }
 
-    String getCharNumberString(String input) {
+    static String getCharNumberString(String emailNames) {
         Map<String, Integer> map = new HashMap<>();
-        Arrays.stream(input.split(""))
-                .forEach(item->{
-                    if(map.containsKey(item)){
-                        map.replace(item, map.get(item) + 1);
-                    }else{
+        Arrays.stream(emailNames.split(""))
+                .forEach(item -> {
+                    if (map.containsKey(item)) {
+                        map.put(item, map.get(item) + 1);
+                    } else {
                         map.put(item, 1);
                     }
                 });
+
+        return formatAsString(map);
+    }
+
+    private static String formatAsString(Map<String, Integer> map) {
         return map.entrySet().stream()
                 .sorted(Map.Entry.comparingByKey())
                 .map(item -> item.getValue() + "(" + item.getKey() + ")")
