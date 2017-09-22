509026a41389230e3ff0f65209ea80f72ad8a190
wang yuanbo <ybowang@CNybowang.local>
2017-09-22 18:12:06 +0800

    [Yuanbo] modify evenSum & wordLength method to Get

--- src/main/java/com/thoughtworks/future_star/api/CalculatorController.java
+++ src/main/java/com/thoughtworks/future_star/api/CalculatorController.java
@@ -1,18 +1,15 @@
 package com.thoughtworks.future_star.api;
 
 import com.thoughtworks.firstProject.Calculator;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class CalculatorController {
-    @PostMapping("/evenSum")
-    public Integer evenSum(@RequestBody List<Integer> list) {
+    @GetMapping(value = "/evenSum", params = "list")
+    public Integer evenSum(@RequestParam("list") List<Integer> list) {
         return Calculator.sumEvens(list);
     }
 }
--- src/main/java/com/thoughtworks/future_star/api/StringController.java
+++ src/main/java/com/thoughtworks/future_star/api/StringController.java
@@ -1,16 +1,13 @@
 package com.thoughtworks.future_star.api;
 
 import com.thoughtworks.firstProject.Sorter;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class StringController {
-    @PostMapping("/wordLength")
-    public String countWordLengthAsc(@RequestBody String string) {
+    @GetMapping(value = "/wordLength", params = "string")
+    public String countWordLengthAsc(@RequestParam("string") String string) {
         return Sorter.countWordLengthAsc(string);
     }
 }
