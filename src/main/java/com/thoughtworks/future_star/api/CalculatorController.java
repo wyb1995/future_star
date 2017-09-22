8574dc300edba9be0659df77014a47f62abcd949
wang yuanbo <ybowang@CNybowang.local>
2017-09-22 17:27:39 +0800

    [Yuanbo] create POST /cal/evenSum api

---
+++ src/main/java/com/thoughtworks/future_star/api/CalculatorController.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.future_star.api;
+
+import com.thoughtworks.firstProject.Calculator;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cal")
+public class CalculatorController {
+    @PostMapping("/evenSum")
+    public Integer evenSum(@RequestBody List<Integer> list) {
+        return Calculator.sumEvens(list);
+    }
+}
